34d0c6e05e49a1d7be89ec4c85139e906b27b1a4
Caner Mollaoğlu <canermollaoglu@hotmail.com>
2025-02-27 20:16:48 +0300

    Product Guncellendi.

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -10,5 +10,7 @@ namespace SilinecekApp.Entities
     {
         public int ID { get; set; }
         public string? ProductName { get; set; }
+        public string? Description { get; set; }
+        public decimal UnitPrice { get; set; }
     }
 }
